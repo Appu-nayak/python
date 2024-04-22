b116c3b0a4020a35e173dbf71e2c4d9c87c8932b
appu nayak <167209962+Appu-nayak@users.noreply.github.com>
2024-04-22 12:03:48 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,7 @@
+def priya():
+    result"this is my balance"
+test_dict={"fname":priya,"age":50,"address":"salem"}
+print("the original dictionary is:"+str(test_dict))
+res=test_dict['fname']()
+print("the required call result:"+str(res))
+
